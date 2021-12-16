94167a147d3b018f61a38e1bb98a1ebe2c2f84bd
phoekoby <phoekoby@mail.ru>
2021-12-16 21:37:21 +0300

    New Quad Point Tree

--- src/main/java/ru/vsu/cs/quadtree/Point.java
+++ src/main/java/ru/vsu/cs/quadtree/Point.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-final class Point<T> implements Comparable<Point<T>> {
+final class Point<T> {
     private double x;
     private double y;
     private T value;
@@ -15,20 +15,5 @@ final class Point<T> implements Comparable<Point<T>> {
     @Override
     public String toString() {
         return "(" + this.x + ", " + this.y + ")";
-    }
-    @Override
-    public int compareTo(Point<T> point) {
-        if (this.x < point.x) {
-            return -1;
-        } else if (this.x > point.x) {
-            return 1;
-        } else {
-            if (this.y < point.y) {
-                return -1;
-            } else if (this.y > point.y) {
-                return 1;
-            }
-            return 0;
-        }
     }
 }
